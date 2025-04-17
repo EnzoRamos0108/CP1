847db8ac70cfa4bdffbe3106e6f6222b3dc46e55
Enzo <enzoframos10@gmail.com>
2025-04-17 20:10:08 -0300

    questão 2 e colocando o script no html

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,11 @@ console.log(vinhoTinto);  // undefined
 console.log(vinhoBranco);  // vazio
 console.log(vinhoNome);  // Vinho Branco Chardonnay meio-seco
 
-//Questão 2 -
+//Questão 2 - Operadores !=, ==, >=
+
+let garrafasVendidas = 60;
+let garrafasNecessarias = 50;
+
+console.log(garrafasVendidas != garrafasNecessarias);  // vai exibir 'true'
+console.log(garrafasVendidas == garrafasNecessarias);  // vai exibir 'false'
+console.log(garrafasVendidas >= garrafasNecessarias); // vai exibir 'true'
