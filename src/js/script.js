3a358f9e992466437d5e19a337e9f1ed2449c39a
Enzo <enzoframos10@gmail.com>
2025-04-17 20:12:42 -0300

    exibindo o número das questões no console e criando divisores para elas

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,20 @@
 let vinhoTinto;  // undefined
 let vinhoBranco = '';  // vazio
 let vinhoNome = 'Vinho Branco Chardonnay meio-seco';  // valor atribuído
-
+console.log('Questão 1')
 console.log(vinhoTinto);  // undefined
 console.log(vinhoBranco);  // vazio
 console.log(vinhoNome);  // Vinho Branco Chardonnay meio-seco
+console.log('--------------------------------------------')
 
 //Questão 2 - Operadores !=, ==, >=
 
 let garrafasVendidas = 60;
 let garrafasNecessarias = 50;
 
+console.log('Questão 2')
 console.log(garrafasVendidas != garrafasNecessarias);  // vai exibir 'true'
 console.log(garrafasVendidas == garrafasNecessarias);  // vai exibir 'false'
 console.log(garrafasVendidas >= garrafasNecessarias); // vai exibir 'true'
+console.log('--------------------------------------------')
+
