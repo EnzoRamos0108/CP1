36341faaebe749f465c67b441c7baa17dc671566
Gustavo <peagudagustavo@gmail.com>
2025-04-17 20:17:29 -0300

    questão1- resolvendo e aplicando os conceitos

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// Questão 1 - Declaração de variáveis
+let vinhoTinto;  // undefined
+let vinhoBranco = '';  // vazio
+let vinhoNome = 'Vinho Branco Chardonnay meio-seco';  // valor atribuído
+
+console.log(vinhoTinto);  // undefined
+console.log(vinhoBranco);  // vazio
+console.log(vinhoNome);  // Vinho Tinto Reserva
